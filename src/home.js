0bdff7fde26ca6dd2a43599768bb5c8fffca127e
vinayak <khandekarvinayak3210@gamil.com>
2020-09-29 16:16:21 +0530

    first commit

---
+++ src/home.js
@@ -0,0 +1,8 @@
+let homeList = []
+
+let listItem = function(title, details, date, project){
+    this.title = title;
+    this.details = details;
+    this.date = date;
+    this.project = project;
+}
---
+++ src/index.js
@@ -0,0 +1,49 @@
+let input = document.querySelector('input');
+let listArea = document.querySelector('#list-area');
+// let CHECK = `fas fa-check-circle`;
+
+let id = 0;
+let list = [];
+
+document.addEventListener('keyup', (event)=>{
+    if (event.which === 13){
+        let task = input.value;
+        let listItem = document.createElement('div');
+
+        listItem.className = 'list-item';
+        listItem.id = id.toString();
+
+        listItem.innerHTML = `<i class="fa fas fa-circle-o check" ></i>
+                                <p id="text">${task}</p>
+                                <i class="material-icons delet">delete</i>`;
+
+        listArea.appendChild(listItem);
+
+        list.push({
+            id : id,
+            task : task,
+            done : false,
+            delet : false
+        });
+        id++;
+        input.value = '';
+    }
+});
+
+listArea.addEventListener('click', (event)=>{
+    let element = event.target;
+    if(element.classList.contains('check')){
+        doneToDo(element);
+    } else if (element.classList.contains('delet')){
+        delet(element);
+    }
+})
+
+function doneToDo(element){
+    element.classList.toggle('fa-circle-o');
+    element.classList.toggle('fa-check-circle');
+}
+
+function delet(element){
+    element.parentNode.style.display = 'none'
+}
